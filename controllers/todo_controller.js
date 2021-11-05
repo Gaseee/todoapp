336b030604634e504ebd92c6caa4a68b52196caa
ll3m <ll3m@fresnou.org>
2021-11-05 09:52:21 -0700

    completed 4.2 pg 17

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,5 +1,6 @@
 const {sequelize} = require("../models/index");
 const {QueryTypes} = require("sequelize");
+const {Todo} = require('../models/index');
 
 module.exports.homeRoute = async function(req, res, next) {
     let toDoItems = await sequelize.query('select * from todo', {type: QueryTypes.SELECT});
